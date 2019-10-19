ceb4dd62cbb600f74cd10c202ba205a99a1c7983
Wills Blake <richwblake@gmail.com>
2019-10-19 16:06:54 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -77,7 +77,7 @@ def gross_per_studio(collection)
   
   # Setting up studio hash with studio names
   while director_index < source.size do
-    if !( studio_gross_totals.has_key? source[director_index][:studio] )
+    if !( studio_gross_totals.has_key? 'source[director_index][:studio]' )
       studio_gross_totals[source[director_index][:studio]] = 0
     end
     director_index += 1
